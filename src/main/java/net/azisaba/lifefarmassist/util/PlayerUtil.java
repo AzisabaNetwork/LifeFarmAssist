d3cc104d7c898b2073c6ab11ad6f3896c3049856
sysnote8main <sysnote8main@gmail.com>
2025-03-26 22:40:16 +0900

    feat: make a detect method simple

--- src/main/java/net/azisaba/lifefarmassist/util/PlayerUtil.java
+++ src/main/java/net/azisaba/lifefarmassist/util/PlayerUtil.java
@@ -1,13 +1,16 @@
 package net.azisaba.lifefarmassist.util;
 
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.EntityEquipment;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.stream.Stream;
 
 public class PlayerUtil {
     public static boolean wearingMythicItem(@NotNull Player player, @NotNull String mythicType) {
-        return mythicType.equals(ItemUtil.getMythicType(player.getInventory().getHelmet())) ||
-                mythicType.equals(ItemUtil.getMythicType(player.getInventory().getChestplate())) ||
-                mythicType.equals(ItemUtil.getMythicType(player.getInventory().getLeggings())) ||
-                mythicType.equals(ItemUtil.getMythicType(player.getInventory().getBoots()));
+        EntityEquipment equipment = player.getEquipment();
+        if(equipment == null) return false;
+        return Stream.of(equipment.getHelmet(), equipment.getChestplate(), equipment.getLeggings(), equipment.getBoots())
+                .anyMatch(stack -> mythicType.equals(ItemUtil.getMythicType(stack)));
     }
 }
